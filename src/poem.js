0276848175b4f55f9ad8548bf331e59fb984a4a2
Joyce Cheung <Jcheung8385@gmail.com>
2024-10-30 15:28:20 +0000

    added javascript event listener so that when a user input a food type the food poem will be displayed

--- src/poem.js
+++ src/poem.js
@@ -1,1 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemContainer = document.querySelector("#poem-verse");
+  poemContainer.innerHTML = `I like sizzling sausages.
+          <br />
+          I like bubbling beans.`;
+}
+
+let poemFormElement = document.querySelector("#food-poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
