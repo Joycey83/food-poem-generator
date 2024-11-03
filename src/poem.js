14c72ca144c580b815924dc6022c06ad3b691359
Joyce Cheung <Jcheung8385@gmail.com>
2024-11-03 11:12:04 +0000

    amended the context so that the poem is on each line not a big paragrapgh

--- src/poem.js
+++ src/poem.js
@@ -11,9 +11,9 @@ function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "a3a5bbeff1t6546488o90285fa5b0853";
-  let prompt = `user instructions: Generate a funny poem about ${instructionsInput.value}. Each sentence on a separate line`;
+  let prompt = `user instructions: Generate a funny poem about ${instructionsInput.value}`;
   let context =
-    "You are a food connoisseur who loves cooking and eating great food around the world. You love to write funny short poems about food. Your mission is to generate a 5 line poem. Make sure to follow the users instructions";
+    "You are a food connoisseur who loves cooking and eating great food around the world. You love to write funny short poems about food. Your mission is to generate a 5 line poem and separate each line with a <br /> Make sure to follow the users instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   // make API call
