6c9ae05314b49e66f0737c84e675491658061084
Joyce Cheung <Jcheung8385@gmail.com>
2024-11-03 10:40:05 +0000

    added the axios cdn and created the display poem function

--- src/poem.js
+++ src/poem.js
@@ -1,18 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let apiKey = "a3a5bbeff1t6546488o90285fa5b0853";
-  let apiURL =
-    "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
-
-  new Typewriter("#poem-verse", {
-    strings: `I like sizzling sausages.
-          <br />
-          I like bubbling beans.`,
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "a3a5bbeff1t6546488o90285fa5b0853";
+  let prompt = "";
+  let context = "";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  // make API call
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#food-poem-generator");
