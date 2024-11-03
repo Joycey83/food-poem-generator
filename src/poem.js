d1c987dbdc44e00146981f185e76a9a0dae8e18b
Joyce Cheung <Jcheung8385@gmail.com>
2024-11-03 11:14:37 +0000

    amending the context for the AI

--- src/poem.js
+++ src/poem.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "a3a5bbeff1t6546488o90285fa5b0853";
   let prompt = `user instructions: Generate a funny poem about ${instructionsInput.value}`;
   let context =
-    "You are a food connoisseur who loves cooking and eating great food around the world. You love to write funny short poems about food. Your mission is to generate a 5 line poem and separate each line with a <br /> Make sure to follow the users instructions";
+    "You are a food connoisseur who loves cooking and eating great food around the world. You love to write funny short poems about food. Your mission is to generate a 5 line poem in basic HTML and separate each line with a <br/>.Make sure to follow the users instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   // make API call
