d216f1d8f906be7419bb080710a3eb4aba8adfbd
Joyce Cheung <Jcheung8385@gmail.com>
2024-11-03 10:33:50 +0000

    added api key and api url

--- src/poem.js
+++ src/poem.js
@@ -1,6 +1,10 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  let apiKey = "a3a5bbeff1t6546488o90285fa5b0853";
+  let apiURL =
+    "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}";
+
   new Typewriter("#poem-verse", {
     strings: `I like sizzling sausages.
           <br />
